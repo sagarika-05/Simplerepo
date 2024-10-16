aff6f2f1072f8527ae6c8ef59c7dc0c05be09ba4
unknown <Sagarika517>
2024-10-16 15:18:04 +0530

    Twotable file commited

---
+++ Exfortwotable.java
@@ -0,0 +1,14 @@
+package LoopingStatements;
+
+public class Exfortwotable {
+	public static void main(String args[])
+	{
+		int a=7;
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(a + " * " + i + " = "
+                    + a * i);
+		}
+	}
+
+}
